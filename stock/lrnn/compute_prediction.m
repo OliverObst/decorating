9dc2972b7a38be054f5d47d42408040e107daaea
Frieder Stolzenburg <fstolzenburg@hs-harz.de>
2022-09-14 09:47:10 +1000

    validation period before training period

--- stock/lrnn/compute_prediction.m
+++ stock/lrnn/compute_prediction.m
@@ -2,37 +2,36 @@
 
 % prerequisites
 addpath('../../lrnn/');
-trainlen = 250;
 evallen = 50;
+trainlen = 250;
 testlen = 50;
 trials = 1000;
 
 % read in stock data
 n = 37; % number of stocks
-[traindat,restdat,names] = read_in_data(trainlen,evallen+testlen);
+[learndat,testdat,names] = read_in_data(evallen+trainlen,testlen);
 
 % parameter setting
 p = 0.1; % relative error
 Nres = 20; % reservoir size
-Next = 0; % extra random dimensions
 result = zeros(37,5);
 
 % prediction of single stock from single/all stocks
 for k=1:n#4=BASF
   k##
-  In = traindat(k,:);
-  Eval = restdat(k,1:evallen); % validation data;
-  Test = restdat(k,evallen+1:end); % test data
-  #hold off; 
-  #plot([In Eval Test],'k'); hold on; % real data sequence
+  Eval = learndat(k,1:evallen); % validation data;
+  In = learndat(k,evallen+1:end); % training data - single stock
+  Range = 1; Seq = In; % training sequence - single stock
+  #Range = k; In = traindat(:,evallen+1:end) % training sequence - all stocks
+  Test = testdat(k,:); % test data
+  #hold off;
+  #plot([Eval In Test],'k'); hold on; % real data sequence
   
   evalerr = +Inf; % best validation error so far
   for t=1:trials
-    R = randn(Next,trainlen);
-    #Range = k; Seq = [traindat(:,:); R]; % training sequence - all stocks
-    Range = 1; Seq = [In; R]; % training sequence - single stock
-    [Outx,Err,Ax,Jx,Yx,Wx,Xx] = predict(Seq,evallen+testlen,Nres,p*mean(In),delta=0,Range);
-    evalerrx = rmse(Eval,Outx(trainlen+1:trainlen+evallen))/mean(In); % actual validation error
+    [Outx,Err,Ax,Jx,Yx,Wx,Xx] = predict(Seq,testlen,Nres,p*mean(In),delta=0,Range);
+    Backx = (Ax*compute(inv(Jx),Yx(:,1),evallen))(Range,end:-1:2); % reverse computation
+    evalerrx = rmse(Eval,Backx)/mean(In); % actual validation error
     if evalerrx<evalerr
       evalerr = evalerrx;
       Out = Outx;
@@ -41,23 +40,19 @@ for k=1:n#4=BASF
       Y = Yx;
       W = Wx;
       X = Xx;
+      Back = Backx;
     endif 
   endfor
   
   tra = result(k,1) = rmse(In,Out(1:trainlen))/mean(In) % relative training error
-  Raw = compute(W,X(:,1),evallen+testlen,Seq);
+  Raw = compute(W,X(:,1),testlen,Seq);
   raw = result(k,2) = rmse(Test,Raw(Range,end-testlen+1:end))/mean(In) % relative testing error
-  #plot(Raw(Range,:),'b'); % without network size reduction
-  #X = [X(:,1:end-1) compute(W,X(:,end),evallen)];
-  #Y = compute(J,start(columns(J)),trainlen+evallen+testlen-1); % output generating mode
-  #A = X/Y(:,1:trainlen+evallen);
-  #Dim = A(Range,:)*Y;
-  Dim = Out;
+  #plot([Eval Raw(Range,:)],'b'); % without network size reduction
+  Dim = [Back Out];
   dim = result(k,3) = rmse(Test,Dim(end-testlen+1:end))/mean(In) % relative testing error
   #plot(Dim,'r'); % with network size reduction
   num = result(k,4) = columns(A) % size of reduced network
   eva = result(k,5) = evalerr
-  #eva2 = result(k,5) = rmse(Eval,Dim(trainlen+1:trainlen+evallen))/mean(In) % relative validation error 
   #legend('real price','predicted price','reduced dimensions');
   #xlabel('day (from 2020-10-27 to 2021-12-30)');
   #ylabel('adjusted ending price in € of BAS.DE (BASF)');
