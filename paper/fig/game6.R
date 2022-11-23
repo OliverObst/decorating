519242c7cf8363e2ce8398379e21174f7369f349
Oliver Obst <o.obst@westernsydney.edu.au>
2022-11-23 23:17:29 +1100

    updated soccer figure

--- paper/fig/game6.R
+++ paper/fig/game6.R
@@ -3,7 +3,7 @@
 #Out2 <- read.csv('../../../examples/robocup/game6out2.csv', header=F)
 library(R.matlab)
 
-mydata <- readMat('../../../examples/robocup/game6.mat')
+mydata <- readMat('game6.mat')
 
 x1 <- mydata$In[1,] # as.numeric(Data[1,])
 y1 <- mydata$In[2,] # as.numeric(Data[2,])
@@ -17,5 +17,5 @@ postscript(file = "game6.eps", width = 12, height = 8)
 plot(x2, y2, type='l', lwd=3, lty=1, col='blue', xlab='x position [m]', ylab='y position [m]', xlim=c(-55,55), ylim=c(-45,40), cex.axis=1.6, cex.lab=1.5, panel.first=grid())
 lines(x3,y3, lwd=3, lty=2, col='red')
 lines(x1,y1, lwd=5, lty=3, col='black')
-legend("bottomright", legend=c("original ball trajectory", "PrNN prediction", "reduced PrNN prediction"), lwd=4, lty=c(3,1:2), col=c("black", "blue", "red"), cex=1.4)
+legend("bottomright", legend=c("original ball trajectory", "LRNN prediction", "reduced LRNN prediction"), lwd=4, lty=c(3,1:2), col=c("black", "blue", "red"), cex=1.4)
 dev.off()
