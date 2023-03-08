7d451467d1f6f1e5eb9951cddafc63550025a492
Oliver Obst <o.obst@westernsydney.edu.au>
2023-03-08 16:01:19 +1100

    added mso arima

--- mso8/arima/mso8arima.py
+++ mso8/arima/mso8arima.py
@@ -9,11 +9,31 @@ Created on Thu Nov 24 00:21:36 2022
 import numpy as np
 from darts import TimeSeries
 from darts.models import AutoARIMA
-from darts.metrics.metrics import mse
+from darts.metrics.metrics import mse, rmse
 import matplotlib.pyplot as plt
 
-N = 20
-results = np.zeros((N,3))
+N = 1
+results = np.zeros((N,5))
+
+def plot_predicted_actual(data, prediction, name = None, filename = None):
+    title = 'MSO8'
+    if isinstance(name, str):
+        title += ': ' + name
+    plt.title(title)
+    plt.ylabel('y')
+    plt.grid(True)
+    plt.autoscale(axis='x', tight=True)
+    
+    plt.plot(data)
+    plt.plot(prediction)
+            
+    if filename is None:
+        plt.show()
+    else:
+        plt.savefig(filename, dpi=300, bbox_inches='tight')
+        
+    return plt
+
 
 for n in range(N):
     filename = f"../data/signal{n+1:02d}.csv"
@@ -24,18 +44,21 @@ for n in range(N):
     series = TimeSeries.from_values(data)
     series, _ = series.split_before(300)
 
-    train, test = series.split_before(250)
+    train_val, test = series.split_before(250)
+    train, val = train_val.split_before(200)
 
     model_arima = AutoARIMA()
-    model_arima.fit(train)
-    prediction_arima = model_arima.predict(len(test))
+    model_arima.fit(train_val)
+    yhat = model_arima.predict(len(test))
 
-    #series.plot(label='actual')
-    #prediction_arima.plot(label='forecast', lw=3)
-    #plt.legend()
+    p = plot_predicted_actual(test.values(copy=True), yhat.values(copy=True),
+                              name = None, filename = f"signal{n+1:02d}.png")
+    p.close()
+    
     results[n,0] = n+1
-    results[n,1] = series.mean(axis=0).values(0)[0][0]
-    results[n,2] = np.sqrt(mse(series, prediction_arima, intersect = True))
+    results[n,1] = train_val.mean(axis=0).values()[0][0]
+    results[n,2] = test.mean(axis=0).values()[0][0]
+    results[n,3] = mse(test, yhat, intersect = True)
+    results[n,4] = rmse(test, yhat, intersect = True)
 
-np.savetxt('results.csv', results, delimiter=',')
-
+#np.savetxt('results-mso8-arima.csv', results, delimiter=',', fmt='%i,%.8f,%.8f,%.8f,%.8f')
