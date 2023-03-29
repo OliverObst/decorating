046585ce81e049d488ff1442b4d0b6b252e0b50d
Oliver Obst <o.obst@westernsydney.edu.au>
2023-03-29 17:13:06 +1100

    plot stock prediction code

---
+++ stock/octave/plotstockprediction.m
@@ -0,0 +1,55 @@
+% Combine the training and test data
+full_dat = [In, Eval, Test];
+
+% Create a time vector for the full dataset
+time_full = 1:length(full_dat);
+
+% Create a time vector for the prediction, starting at step 251
+time_pred = 251:length(Out);
+
+% Set up the figure and axis with a white background
+figure('Color', 'w');
+ax = gca;
+hold on;
+
+% Plot the true data from step 251
+plot(time_pred, full_dat(251:end), 'LineWidth', 2, 'Color', 'b', 'DisplayName', 'True Data');
+
+% Plot the predicted data
+plot(time_pred, Out(251:end), 'LineWidth', 2, 'Color', 'r', 'LineStyle', '--', 'DisplayName', 'Predicted Data');
+
+% Configure the main axis
+set(ax, 'FontSize', 14);
+set(ax, 'LineWidth', 1.5);
+set(ax, 'Box', 'on');
+xlabel('Time Step', 'FontSize', 16);
+ylabel('Value', 'FontSize', 16);
+title('Stock Prediction: FME', 'FontSize', 18);
+
+% Add a legend
+legend('show', 'Location', 'best');
+
+% Create inset axes
+inset_pos = [0.15, 0.14, 0.3, 0.22]; % [left, bottom, width, height]
+ax_inset = axes('Position', inset_pos);
+box on;
+hold on;
+
+% Plot the full data in the inset
+plot(ax_inset, time_full, full_dat, 'LineWidth', 1.5, 'Color', 'b', 'DisplayName', 'True Data');
+plot(ax_inset, time_pred, Out(251:end), 'LineWidth', 1.5, 'Color', 'r', 'LineStyle', '--', 'DisplayName', 'Predicted Data');
+
+% Configure the inset axis
+set(ax_inset, 'FontSize', 10);
+set(ax_inset, 'LineWidth', 1);
+xticklabels(ax_inset, []);
+yticklabels(ax_inset, []);
+% xlabel(ax_inset, 'Time Step', 'FontSize', 12);
+% ylabel(ax_inset, 'Value', 'FontSize', 12);
+title(ax_inset, 'Full Series Overview', 'FontSize', 12);
+% xlim(ax_inset, [1, length(full_dat)]);
+
+% Save the figure to a high-quality image
+print('-dpng', '-r300', 'timeseries_prediction.png');
+
+hold off;
